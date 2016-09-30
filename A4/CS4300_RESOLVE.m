ae2bcd86d282ab52d78dfd442710a1a89a888b42
24leesten <l.stenquist@utah.edu>
2016-09-29 19:55:25 -0600

    I made changes to fix the resolve function

--- A4/CS4300_RESOLVE.m
+++ A4/CS4300_RESOLVE.m
@@ -1,4 +1,4 @@
-function resolved = CS4300_RESOLVE(Ci,Cj)
+function resolvents = CS4300_RESOLVE(Ci,Cj)
 % CS4300_RESOLVE - resolution theorem prover
 % On input:
 %     Ci (1xn vector): a clause
@@ -16,33 +16,26 @@ function resolved = CS4300_RESOLVE(Ci,Cj)
 %     Fall 2016
 %
 
-resolved = [];
+% We want to returns all resolvents. A resolvent is two clauses that are
+% ORed together after we have found a two leterals that contradict. There
+% may be multiple resolvents.
 
+
+resolvents = [];
+count = 0;
 % resolve ci, cj
 for k = 1:length(Ci)
-    conflict = 0;
+    %resolvents(k).clauses = Ci;    
     for l = 1:length(Cj)
         if Ci(k) + Cj(l) == 0
-            conflict = 1;
-            break;
+            count = count + 1;
+            Ci_t = Ci;
+            Cj_t = Cj;
+            Ci_t(k) = [];
+            Cj_t(l) = [];
+            resolvents(count).clauses = [Ci_t Cj_t]; 
+            % union the clauses without the conflict
         end
-    end
-    
-    if ~conflict
-        resolved = [resolved Ci(k)];
     end
 end
 
-for l = 1:length(Cj)
-    conflict = 0;
-    for k = 1:length(Ci)
-        if Ci(k) + Cj(l) == 0
-            conflict = 1;
-            break;
-        end
-    end
-    
-    if ~conflict && ~ismember(Cj(l),resolved)
-        resolved = [resolved Cj(l)];
-    end
-end
