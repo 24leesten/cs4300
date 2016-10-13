50b18f60b4a7e36c5cecb5e60778cc2a9c26c1b0
Ryan Franklin Keepers <keepers@lab2-11.eng.utah.edu>
2016-10-13 09:33:30 -0600

    added make percept tell function

---
+++ A4b/CS4300_MPS.m
@@ -0,0 +1,18 @@
+function sentence = CS4300_MPS(percept)
+% CS3400_cnf_contains - does the cnf contain the clause
+% On input:
+%     percept (1x5 num vector): a vector of boolean percepts
+% On output:
+%     sentence (CNF data structure): array of conjuctive clauses
+%       (i).clauses
+%           each clause is a list of integers (- for negated literal)
+%     clause (CNF datastructure): a conjunct of disjunctive clauses to be tested
+% Call: 
+%     percept = [0,0,1,0,1];
+%     sentence = CS3400_MPS(percept);
+% Author:
+%     Ryan Keepers
+%     Leland Stenquist
+%     UU
+%     Fall 2016
+%
