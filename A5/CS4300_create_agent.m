766c459bc52e947aaa35dd72ff62bbe90d0e0c0a
Ryan Franklin Keepers <keepers@lab2-12.eng.utah.edu>
2016-10-25 19:27:32 -0600

    function to convert state into an agent

---
+++ A5/CS4300_create_agent.m
@@ -0,0 +1,24 @@
+function agent = CS4300_create_agent(state)
+% CS4300_create_agent - create a hybrid agent struct
+% On input:
+%     state (1x3 array): representing the state of the agent
+%         (1): x location of the agent
+%         (2): y location of the agent
+%         (3): direction the agent is facing
+% On output:
+%     agent (struct): the hybrid agent
+%         x: x location of the agent
+%         y: y loaction of the agent
+%         dir: direction the agent is facing
+% Call:
+% action = CS4300_MC_HWA([1 0 0 1 0]);
+% Author:
+%     Ryan Keepers
+%     Leland Stenquist
+%     UU
+%     Fall 2016
+%
+
+agent.x = state(1);
+agent.y = state(2);
+agent.dir = state(dir);
