117f6e02fd3c27ac68b398edb19a664efc6e8a43
24leesten <l.stenquist@utah.edu>
2016-11-17 19:08:25 -0700

    testing

---
+++ A7/CS4300_summation_test.m
@@ -0,0 +1,48 @@
+function CS4300_summation_test()
+
+
+% Test 1
+state_probs = [0.1 0.1 0 0 0.8 0 0 0 0 0 0 0];
+U = [0.705 0.655 0 0 0.762 0 0 0 0 0 0 0];
+m = CS4300_summation(state_probs,U);
+
+if (m == 0.7456)
+    display('TEST 1: PASSED')
+else
+    display('TEST 1: FAILED')
+end
+
+
+% Test 2
+state_probs = [0.9 0 0 0 0.1 0 0 0 0 0 0 0];
+U = [0.705 0.655 0 0 0.762 0 0 0 0 0 0 0];
+m = CS4300_summation(state_probs,U);
+
+if (m == 0.7107)
+    display('TEST 2: PASSED')
+else
+    display('TEST 2: FAILED')
+end
+
+% Test 3
+state_probs = [0.9 0.1 0 0 0 0 0 0 0 0 0 0];
+U = [0.705 0.655 0 0 0.762 0 0 0 0 0 0 0];
+m = CS4300_summation(state_probs,U);
+
+if (m == 0.7)
+    display('TEST 3: PASSED')
+else
+    display('TEST 3: FAILED')
+end
+
+% Test 4
+state_probs = [0.1 0.8 0 0 0.1 0 0 0 0 0 0 0];
+U = [0.705 0.655 0 0 0.762 0 0 0 0 0 0 0];
+m = CS4300_summation(state_probs,U);
+
+if (round(m,4) == 0.670700)
+    display('TEST 4: PASSED')
+else
+    display('TEST 4: FAILED')
+end
+
