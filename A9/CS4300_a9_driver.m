e0969faf2331c3a12784d1119151cdaf6e998f95
Ryan Franklin Keepers <keepers@lab2-13.eng.utah.edu>
2016-12-02 12:08:31 -0700

    a9 start

---
+++ A9/CS4300_a9_driver.m
@@ -0,0 +1,14 @@
+function CS4300_a9_driver
+% CS4300_a9_driver - drives a9
+% On input:
+%     not_used (int): I don't actually use this
+% On output:
+%     no_output (int): Doesn't actually put out
+% Call:
+%     CS4300_a9_driver;
+% Author:
+%     Ryan Keepers
+%     Leland Stenquist
+%     UU
+%     Fall 2016
+%
